465375d448e1634a6a3d916a309b36c2965dfcfa
mikhael zimmermann <mikhael928@gmail.com>
2023-05-30 12:35:19 +0200

    Start the user update

--- db/migrate/20230529112508_devise_create_users.rb
+++ db/migrate/20230529112508_devise_create_users.rb
@@ -7,6 +7,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## User personal information
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :location
+
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -31,7 +37,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_135741) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "location"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
