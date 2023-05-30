d7ee419180bf679acf92deb6028f2166f4cdbf5d
Niklas Springer <me@niklaspringer.xyz>
2023-05-30 12:56:18 +0200

    cloudinary config added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
