7ea3cb836bf83302ec5e432ffafeb462b48db427
Niklas Springer <me@niklaspringer.xyz>
2023-06-02 14:47:43 +0200

    update fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'bookings/show'
 
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users
   root to: "listings#index"
   resources :listings do
     resources :bookings, only: [:new, :create, :show]
